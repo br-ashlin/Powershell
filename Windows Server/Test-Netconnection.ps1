00f01162a7c895d4e1acc15d3378191def9ec551
Ben Ashlin <brashlin@outlook.com>
2021-12-13 13:37:48 +1000

    Create Test-Netconnection.ps1

---
+++ Windows Server/Test-Netconnection.ps1
@@ -0,0 +1,11 @@
+#Bulk Server and Port Network Scanning
+
+$Ports  = "135","443", "5286" 
+$Servers = Get-Content servers.txt 
+ForEach($Server in $Servers)
+{
+Foreach ($P in $Ports){
+$check=Test-NetConnection $server -Port $P -WarningAction SilentlyContinue
+$check | Select ComputerName, PingSucceeded, RemotePort, TcpTestSucceeded
+}
+}
